42e04eeed576be1f167e5434266dbbe8b819ffd5
gyeongminpark <mh7840@naver.com>
2024-04-16 11:48:00 +0900

    기초환경 세팅

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+import {menu} from "./menu.js"
+import {intro} from "./intro.js"
+import {graphics} from "./graphics.js"
+import {link} from "./link.js"
+import {smooth} from "./smooth.js"
+import {work} from "./work.js"
+
+window.addEventListener('load',function(){
+    smooth()
+    link()
+    menu()
+    intro()
+    work()
+    graphics()
+})
