30b3ac6b2c0541a5bec9e6192ebee36caf7cb682
gyeongminpark <mh7840@naver.com>
2024-04-17 14:37:54 +0900

    02.페이지완료

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,3 +1,20 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 export function intro (){
-    
+    gsap.registerPlugin(ScrollTrigger)
+    const frameCount=9
+    let offsetValue=360
+
+
+        gsap.to('.img',{
+            backgroundPosition:(-offsetValue*frameCount)+'px',
+            ease:'steps('+frameCount+')',
+            scrollTrigger:{
+                trigger:'#intro',
+                start:'top top',
+                end:'+='+(frameCount * offsetValue),
+                pin:true,
+                scrub:true
+            }
+        })
 }
